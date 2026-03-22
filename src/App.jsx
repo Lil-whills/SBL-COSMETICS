c4150840b1314256d91a44749a01cb7e8f931608
Ameyaw Williams Kyere <williams.kyere@stu.ucc.edu.gh>
2026-03-22 03:19:07 +0000

    Set up React with Vite and Tailwind CSS v4

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full bg-blue-700 h-screen'>
+      <h1 className='text-white text-4xl font-bold text-center mt-20'>Welcome to SBL COSMETICS!</h1>
+    </div>
+  )
+}
+
+export default App
